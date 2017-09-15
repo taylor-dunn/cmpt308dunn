3e3c7e4d97d1ead35060f723eb8e38cb1a3acff8
taylor-dunn <31493105+taylor-dunn@users.noreply.github.com>
2017-09-15 15:38:14 -0400

    Taylor Dunn, 9.15.17, Lab 3 (trial)
    not sure how to save SQL from pgadmin 4 as a sql file to a normal folder on my computer

---
+++ Labs/Lab3/lab3.sql
@@ -0,0 +1,54 @@
+﻿--
+--Lab 2
+--
+--Taylor Dunn
+
+select * 
+from orders;
+
+select ordno, totalusd
+from orders;
+
+select * 
+from agents;
+
+select name, city 
+from agents 
+where name = 'Smith';
+
+select *
+from products;
+
+select pid, name, priceusd
+from products
+where qty > 200010;
+
+select * 
+from customers;
+
+select name, city
+from customers
+where city = 'Duluth';
+
+select name 
+from agents
+where city != 'New York' AND city != 'Duluth';
+
+select * 
+from products 
+where city != 'Duluth'AND city != 'Dallas' AND 
+priceusd >= 1.00;
+
+select *
+from orders 
+where month = 'Mar' OR month = 'May';
+
+select *
+from orders 
+where month = 'Feb' AND totalusd >=500.00;
+
+select *
+from orders
+where cid = 'c005';
+
+
