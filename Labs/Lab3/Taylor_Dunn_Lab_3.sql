a5f78fec3e381ca0a04a33c62e5e5e19b5d4af10
taylor-dunn <31493105+taylor-dunn@users.noreply.github.com>
2017-09-18 08:08:20 -0400

    simple changes to formatting of SQL file

--- Labs/Lab3/Taylor_Dunn_Lab_3.sql
+++ Labs/Lab3/Taylor_Dunn_Lab_3.sql
@@ -1,52 +1,64 @@
 ﻿--
---Lab 2
+--Lab 3
 --
 --Taylor Dunn
 
 select * 
 from orders;
 
-select ordno, totalusd
-from orders;
-
-select * 
-from agents;
-
-select name, city 
-from agents 
-where name = 'Smith';
-
 select *
 from products;
-
-select pid, name, priceusd
-from products
-where qty > 200010;
 
 select * 
 from customers;
 
+select * 
+from agents;
+
+-- 1 --
+select ordno, totalusd
+from orders;
+
+
+-- 2 --
+select name, city 
+from agents 
+where name = 'Smith';
+
+
+-- 3 --
+select pid, name, priceusd
+from products
+where qty > 200010;
+
+
+-- 4 --
 select name, city
 from customers
 where city = 'Duluth';
 
+-- 5 --
 select name 
 from agents
 where city != 'New York' AND city != 'Duluth';
 
+-- 6 --
 select * 
 from products 
 where city != 'Duluth'AND city != 'Dallas' AND 
 priceusd >= 1.00;
 
+-- 7 --
 select *
 from orders 
 where month = 'Mar' OR month = 'May';
 
+-- 8 --
 select *
 from orders 
 where month = 'Feb' AND totalusd >=500.00;
 
+-- 9 --
 select *
 from orders
 where cid = 'c005';
