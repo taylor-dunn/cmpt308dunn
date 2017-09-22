73081937ddea8a8cc16437d84b9a87d39d881258
taylor-dunn <31493105+taylor-dunn@users.noreply.github.com>
2017-09-22 13:19:39 -0400

    Taylor Dunn, 9.22.17, Lab 4

--- Labs/Lab4/lab4.sql
+++ Labs/Lab4/lab4.sql
@@ -43,3 +43,55 @@ where cid in
     select cid 
     from orders 
     where pid = 'p07');
+    
+--5--
+select distinct pid 
+from products
+where pid not in (
+     select pid
+     from orders
+     where aid in (
+    	select aid 
+    	from orders
+        where aid ='a02'
+        or aid = 'a03'))
+		order by pid DESC;
+      
+--6--     
+select distinct name, discountpct, city 
+from customers 
+where cid in
+	(select distinct cid 
+    from orders 
+    where aid in (
+        select distinct aid
+        from agents 
+        where city =
+        'Tokyo' or city ='New York'));
+        
+--7--
+select * 
+from customers
+where  not city = 'Duluth' 
+intersect 
+select *
+from customers 
+where not city = 'London' and 
+discountpct in (
+    select discountpct 
+    from customers  
+     where city = 'Duluth' or city = 'London');
+     
+--8--     
+Check constraints are limitations that put a control to the values that are accepted by the columns 
+being returned. A check constraint can be used with any logical expression that returns True or False.
+Check constraints are useful because they control the data being outputted, in turn increasing dsta validity
+and integrity. It is also very helpful because it enforces rules throughout the database, without 
+adding additional logic to the application. Some good uses of a test constraint would be when trying to only
+find the salary between two values, you could insert salary >= 15000 AND salary <= 100000. This will only 
+return values between the two, and give valid data that is wanted. A bad test constraint would be one where 
+you check for values and leave out important ones, or maybe include too much information when it could have 
+been limited even more.
+
+http://www.datanamic.com/support/checkconstraintbenefits.html
+https://technet.microsoft.com/en-us/library/ms188258(v=sql.105).aspx
